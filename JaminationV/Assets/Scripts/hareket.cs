31d680094f03c92669ed3c95b87dd398a79c725a
alitemz <106404814+alitemz@users.noreply.github.com>
2022-05-28 02:42:59 +0300

    deneme ali

---
+++ JaminationV/Assets/Scripts/hareket.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class hareket : MonoBehaviour
+ 
+{
+      private Rigidbody rb;
+
+[SerializeField]
+    int Hiz = 10;
+
+
+    
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); ///fiziksel özellikleri alýnmýþ
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float yatay = Input.GetAxis("Horizontal");
+
+        float dikey = Input.GetAxis("Vertical");
+        Vector3 hareket = new Vector3(yatay * Hiz, 0.0f, dikey * Hiz);
+        rb.AddForce(hareket);//force güç ekle demek
+    }
+
+}
